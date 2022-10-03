5ad0f6a312d5c82a06472fc3f56ccc813476eb5d
mati <maticolis1999_sacchi@hotmail.com>
2022-10-03 20:33:45 -0300

    update

---
+++ angular/proyectoBiblioteca/src/app/Servicios/usuario.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  url = 'http://localhost:8081/api/users';
+
+
+
+
+
+  constructor() {
+
+  
+
+   }
+}
