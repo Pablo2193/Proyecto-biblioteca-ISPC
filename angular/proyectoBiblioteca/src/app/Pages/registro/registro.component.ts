64a450888e31bda34f98bd4b7171d9650d04d038
Angeles Franco <angelesdfranco91@gmail.com>
2022-10-04 22:38:53 -0300

    Cargo al repositorio paginas de Login y Registro

---
+++ angular/proyectoBiblioteca/src/app/Pages/registro/registro.component.ts
@@ -0,0 +1,21 @@
+// register.component.ts
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-register",
+  templateUrl: "./registro.component.html",
+  styleUrls: ["./registro.component.css"]
+})
+export class RegistroComponent {
+  email!: string;
+  password!: string;
+  confirmPassword!: string;
+
+  constructor() {}
+
+  register() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+}
--- angular/proyectoBiblioteca/src/app/app.module.ts
+++ angular/proyectoBiblioteca/src/app/app.module.ts
@@ -1,23 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
-
 import { LayoutComponent } from './Layout/Layout.component';
 import { NavComponent } from './Layout/nav/nav.component';
 import { FooterComponent } from './Layout/footer/footer.component';
 import { HeaderComponent } from './Layout/header/header.component';
+import { PagesComponent } from './Pages/Pages.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegistroComponent } from './Pages/registro/registro.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
     NavComponent,
     HeaderComponent,
     FooterComponent,
-      LayoutComponent
+    LoginComponent,
+    RegistroComponent,
+    LayoutComponent,
+    PagesComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
